37ab3d3cec940d0a4af60fcc48f2ad617d580e69
Mellor91 <j.mellor@live.co.uk>
2017-02-19 20:21:01 +0000

    a lot dont to the middleware... sorted out the QueryGenerator even more.... you can pass values into it to return sql objects or JSON, can use it for regex numbers in string for validation returning true of false, get last inserted key from database (always a bit wary of that as i dont know if high traffic could affect the result) and will handle selects, selects with any join, soft deletes and inserts... not doing updates as im keeping records so will soft delete and make a new record for history sake....... can select user, authenticate user, update nickname, delete user(will probably change this to 'record' so that it can handle any input and soft delete them), enter a new score, create a new user

--- middleware/QueryGenerator.php
+++ middleware/QueryGenerator.php
@@ -1,11 +1,19 @@
  <?php 
 include("DatabaseConnector.php");
 
-//Accepts: Table = String, $columns = Array, $conditions = Array or NULL, $conditions = Array or NULL
-function selectStatement($table, $columns, $conditionColumns, $conditions)
+//Accepts: Table = String, $columns = Array, $conditions = Array or NULL, $conditions = Array or NULL, $joinTableWithCondition = Array or NULL
+function selectStatement($table, $columns, $conditionColumns, $conditions, $joinTableWithCondition)
 {
     $querySkeleton = array("SELECT ", " FROM ");
     $columns = implode(",",$columns);
+    $join = "";
+    $conditionString = "";
+
+    if(isset($joinTableWithCondition))
+    {
+        $join = " ".$joinTableWithCondition[0]." ".$joinTableWithCondition[1]." ON ".$table.".".$joinTableWithCondition[2]." = ".$joinTableWithCondition[1].".".$joinTableWithCondition[2];
+    }
+
 
     if(sizeof($conditionColumns) > 0 && sizeof($conditions) > 0)
     {
@@ -15,39 +23,79 @@ function selectStatement($table, $columns, $conditionColumns, $conditions)
             $i = 0;
             foreach ($conditionColumns as $tmp) 
             {
-                if(is_numeric($conditions[$i]) || strtolower($conditions[$i]) == "true" || strtolower($conditions[$i]) == "false" )
+                if(is_numeric($conditions[$i]) || strtolower($conditions[$i]) == "true" || strtolower($conditions[$i]) == "false")
                 {
                     $conditionString = $conditionString.$conditionColumns[$i]." = ".$conditions[$i]. " AND ";
+                }
+                elseif(strtolower($conditions[$i]) == "null")
+                {
+                    $conditionString = $conditionString.$conditionColumns[$i]." IS NULL AND ";
+                }
+                elseif(strtolower($conditions[$i]) == "not null")
+                {
+                    $conditionString = $conditionString.$conditionColumns[$i]." IS NOT NULL AND ";
                 }
                 else
                 {
                     $conditionString = $conditionString.$conditionColumns[$i]." = '".$conditions[$i]. "' AND ";
                 }
                 $i++;
+                echo "<br>";
             }
-
             $conditionString = substr($conditionString, 0, -5);
-
-            echo $querySkeleton[0].$columns.$querySkeleton[1].$table.$conditionString;
-            echo "<br>";
-            echo toJSON(queryDB($querySkeleton[0].$columns.$querySkeleton[1].$table.$conditionString));
-            return;
-
         }
         else
         {
-            echo "condition column and conditions dont match";
+            die("condition column and conditions dont match in length... did you do a whoopsy????");
+            return;
         }
     }
-    echo $querySkeleton[0].$columns.$querySkeleton[1].$table;
+    echo $querySkeleton[0].$columns.$querySkeleton[1].$table.$join;
     echo "<br>";
-    echo toJSON(queryDB($querySkeleton[0].$columns.$querySkeleton[1].$table));
+    return queryDB($querySkeleton[0].$columns.$querySkeleton[1].$table.$join.$conditionString);
 }
 
 
+function insertStatement($table, $columns, $values)
+{
+    $querySkeleton = array("INSERT INTO ", " (", ") ", " VALUES ");
+    $columns = implode(",",$columns);
+    $valueString = "";
 
+    $i = 0;
+    foreach($values as $v)
+    {
+        if(is_numeric($values[$i]) || strtolower($values[$i]) == "true" || strtolower($values[$i]) == "false" ||  strtolower($values[$i]) == "now()")
+        {
+            $valueString = $valueString.$values[$i].",";
+        }
+        else
+        {
+            $valueString = $valueString."'".$values[$i]."',";
+        }
+        $i++;
+    }
+    $valueString = substr($valueString, 0, -1);
 
+    echo $querySkeleton[0].$table.$querySkeleton[1].$columns.$querySkeleton[2].$querySkeleton[3].$querySkeleton[1].$valueString.$querySkeleton[2];
+    echo "<br>";
 
+    queryDB($querySkeleton[0].$table.$querySkeleton[1].$columns.$querySkeleton[2].$querySkeleton[3].$querySkeleton[1].$valueString.$querySkeleton[2]);               
+}
+
+function softDelete($table, $key, $value)
+{
+    $querySkeleton = array("UPDATE ", " SET ", "WHERE ", " AND deleted = NULL");
+   
+    echo $querySkeleton[0].$table.$querySkeleton[1]."deleted = NOW() ".$querySkeleton[2].$key." = ".$value.$querySkeleton[3];
+    echo "<br>";
+    queryDB($querySkeleton[0].$table.$querySkeleton[1]."deleted = NOW() ".$querySkeleton[2].$key." = ".$value.$querySkeleton[3]);
+}
+
+function toObject($result)
+{
+    return mysqli_fetch_object($result);
+}
 
 function toJSON($result)
 {
@@ -57,4 +105,22 @@ function toJSON($result)
         }
     return json_encode($JSONArray); 
 }
+
+function getLastInsertedKey()
+{
+    return $conn->insert_id;
+}
+
+function numbersInString($string)
+{
+    if(1 === preg_match('~[0-9]~', $string))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 ?>
--- middleware/index.php
+++ middleware/index.php
@@ -1,13 +1,16 @@
  <?php 
-
 include("QueryGenerator.php");
+
+//maybe have a select statement for tables and have them in constants??? thought for food....
+//means that if things change in the database... these wont be affected
+
+//remember to make gets posts
+
 
 if(isset($_GET['selectUser']))
 {
     $table = "users";
-    $columns = array();
-    $conditionColumns = array();
-    $conditions = array();
+    $columns = array("user_id");
 
     if(isset($_GET['column']))
     {
@@ -19,13 +22,117 @@ if(isset($_GET['selectUser']))
         $conditionColumns = $_GET['condition_column'];
         $conditions = $_GET['condition'];
     }
-    selectStatement($table, $columns, NULL, NULL);
+    echo toJSON(selectStatement($table, $columns, NULL, NULL, NULL));
 }
 
 
+if(isset($_GET['authenticateUser']))
+{
+    $table = "users";
+    $columns = array("users.user_id","users.first_name","users.surname", "player_nicknames.nickname");
+    $joinTableWithCondition = array("INNER JOIN", "player_nicknames", "user_id");
+
+    if(isset($_GET['email']) && isset($_GET['password']))
+    {
+        array_push($conditionColumns, "users.email", "users.password", "users.deleted", "player_nicknames.deleted");
+        array_push($conditions, $_GET['email'], MD5($_GET['password']), "NULL", "NULL");
+    }
+    else
+    {
+        die("credentials not supplied");
+    }
+    echo toJSON(selectStatement($table, $columns, $conditionColumns, $conditions, $joinTableWithCondition));
+}
 
 
+if(isset($_GET['updateNickname']))
+{
+    $table = "player_nicknames";
+    $columns = array("nickname_id");
 
+    if(isset($_GET['user_id']) && isset($_GET['current_nickname']) && isset($_GET['new_nickname']))
+    {
+        array_push($conditionColumns, "user_id", "nickname", "deleted");
+        array_push($conditions, $_GET['user_id'], $_GET['current_nickname'], "NULL");
+        
+        $nicknameObj = toObject(selectStatement($table, $columns, $conditionColumns, $conditions, NULL));
+        if($nicknameObj != NULL)
+        {
+            softDelete($table, "nickname_id", $nicknameObj->nickname_id);
+        }
+        else
+        {
+            die("Unable to delete current nickname");
+        }
 
+        $insertColumns = array("user_id", "nickname", "created");
+        $insertValues = array($_GET["user_id"],$_GET["new_nickname"],"NOW()");
+       
+        insertStatement($table, $insertColumns, $insertValues);
+    }
+    else
+    {
+        die("credentials not supplied");
+    }
+}
+
+//TEST WHEN YOU HAVE MADE AN ADD USER
+
+if(isset($_GET['deleteUser']))
+{
+    if(isset($_GET['user_id']))
+    {
+        softDelete("users", "user_id", $_GET['user_id']);
+        softDelete("player_nicknames", "user_id", $_GET['user_id']);
+        softDelete("user_game_scores", "user_id", $_GET['user_id']);
+    }
+    else
+    {
+        die("Unable to delete current nickname");
+    }
+}
+
+if(isset($_GET['newScore']))
+{
+    if(isset($_GET['user_id']) && isset($_GET['map_id']) && isset($_GET['score']))
+    {
+        $insertColumns = array("user_id", "map_id", "score", "created");
+        $insertValues = array($_GET["user_id"],$_GET["map_id"], $_GET["score"], "NOW()");
+       
+        insertStatement("user_game_scores", $insertColumns, $insertValues);
+    }
+    else
+    {
+        die("credentials not supplied");
+    }
+}
+
+if(isset($_GET['newUser']))
+{
+    if(isset($_GET['first_name']) && isset($_GET['surname']) && isset($_GET['email']) && isset($_GET['password']) && isset($_GET['nickname']))
+    {
+        $table = "users";
+        $insertColumns = array("first_name", "surname", "email", "password", "created");
+        $insertValues = array($_GET["first_name"], $_GET["surname"], $_GET["email"], MD5($_GET["password"]), "NOW()");
+        
+        if(numbersInString($_GET["first_name"]) || numbersInString($_GET["surname"]))
+        {
+            die("Names cannot have numbers in");
+        }
+
+        insertStatement("users", $insertColumns, $insertValues);
+
+        $user = toObject(selectStatement($table, array("user_id"), array("email"), array($_GET['email']),NULL));
+
+        $insertColumns = array("user_id", "nickname", "created");
+        $insertValues = array($user->user_id,$_GET["nickname"], "NOW()");
+
+        insertStatement("player_nicknames", $insertColumns, $insertValues);
+    }
+    else
+    {
+        die("credentials not supplied");
+    }
+}
 
 ?>
